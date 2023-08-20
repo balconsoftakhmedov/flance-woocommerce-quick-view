e864b4c16cd0049071f1ba4b8040d9f3178857da
BK <akhmedov@balcomsoft.com>
2023-08-20 14:32:02 +0500

    Update simple.php

--- templates/single-product/save/simple.php
+++ templates/single-product/save/simple.php
@@ -31,7 +31,7 @@ if ( $product->is_in_stock() ) : ?>
 
 	<form class="cart" action="<?php echo esc_url( apply_filters( 'woocommerce_add_to_cart_form_action', $product->get_permalink() ) ); ?>" method="post" enctype='multipart/form-data'>
 		<?php
-		do_action( 'woocommerce_before_add_to_cart_button' ); ?>
+		do_action( 'flance_woocommerce_before_add_to_cart_button' ); ?>
 
 		<?php
 		//do_action( 'woocommerce_before_add_to_cart_quantity' );
